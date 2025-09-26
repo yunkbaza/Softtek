b371289f85d3fb39d22b9336e60eb92a9e9c88af
Allan Baeza <70580298+yunkbaza@users.noreply.github.com>
2025-09-26 19:41:20 -0300

    Update EmotionModel.swift

--- SofttekApp/SofttekApp/Models/EmotionModel.swift
+++ SofttekApp/SofttekApp/Models/EmotionModel.swift
@@ -1,7 +1,16 @@
+//
+//  EmotionModel.swift
+//  SofttekApp
+//
+//  Created by yunkbaza on 26/09/25.
+//
+
 import Foundation
 
-struct Emotion: Identifiable {
-    var id: Int64
-    var emotion: String
-    var timestamp: Date
+// Modelo para representar o check-in de humor (emojis)
+struct EmotionModel: Codable, Identifiable {
+    var id: String? // Opcional, pois o ID é gerado pelo Firestore no back-end
+    let emotion: String
+    let timestamp: Date
+    let userId: String
 }
